d0cd7ac65f424be35bb0309d95ae02caee1002f5
Africa Nerd <34450905+africanerd@users.noreply.github.com>
2019-01-21 11:43:13 -0500

    Update settings.py

--- AfricaNerdGameTictoeApp/settings.py
+++ AfricaNerdGameTictoeApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&y!^hmcr+c7(cj45+(1i%_o^jroc+kqzj8r^_gf+yrdzc!g9+='
+SECRET_KEY = 'ADD YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
